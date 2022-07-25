46470c576c9b58d13dec1024c2f948a0562a2ba0
Tommy Cheal <tommycheal@hotmail.com>
2022-07-25 14:05:43 -0400

    Cleaned Up

--- main.py
+++ main.py
@@ -11,13 +11,9 @@ from grouper import Grouper
 # @IN menu_item_df @URI file:./data/MenuItem.zip
 # @IN menu_page_df @URI file:./data/MenuPage.zip
 # @IN menu_df @URI file:./data/Menu.zip
-# @OUT FullJoin @URI file:./data/FullJoin.csv
-
+# @OUT FullJoin.csv @URI file:./data/FullJoin.csv
 
 def pre_process():
-    # @BEGIN clean
-    # @IN dish_df @IN menu_item_df @IN menu_page_df @IN menu_df
-    # @OUT clean_dish_df @OUT clean_menu_item_df @OUT clean_menu_page_df @OUT clean_menu_df
 
     # @BEGIN _coerce_to_int64
     # @IN dish_df @IN menu_item_df @IN menu_page_df @IN menu_df
@@ -26,24 +22,23 @@ def pre_process():
 
     # @BEGIN _coerce_to_datetime
     # @IN int_dish_df @IN int_menu_item_df @IN int_menu_page_df @IN int_menu_df
-    # @OUT datetime_menu_dish_df @OUT datetime_menu_item_df @OUT datetime_menu_page_df @OUT datetime_menu_df
+    # @OUT datetime_dish_df @OUT datetime_menu_item_df @OUT datetime_menu_page_df @OUT datetime_menu_df
     # @END _coerce_to_datetime
 
     # @BEGIN _drop_non_required_col
     # @IN datetime_dish_df @IN datetime_menu_item_df @IN datetime_menu_page_df @IN datetime_menu_df
-    # @OUT drop_menu_dish_df @OUT drop_menu_item_df @OUT drop_menu_page_df @OUT drop_menu_df
+    # @OUT drop_dish_df @OUT drop_menu_item_df @OUT drop_menu_page_df @OUT drop_menu_df
     # @END _drop_non_required_col
 
     # @BEGIN _delete_rows_with_null_values
     # @IN drop_dish_df @IN drop_menu_item_df @IN drop_menu_page_df @IN drop_menu_df
-    # @OUT clean_menu_dish_df @OUT clean_menu_item_df @OUT clean_menu_page_df @OUT clean_menu_df
+    # @OUT clean_dish_df @OUT clean_menu_item_df @OUT clean_menu_page_df @OUT clean_menu_df
     # @END _delete_rows_with_null_values
 
     dish_df, menu_item_df, menu_page_df, menu_df = create_data_frames()
-    # @END clean
 
-    # @BEGIN Join
-    # @IN clean_menu_dish_df @IN clean_menu_item_df @IN clean_menu_page_df @IN clean_menu_df
+    # @BEGIN join
+    # @IN clean_dish_df @IN clean_menu_item_df @IN clean_menu_page_df @IN clean_menu_df
     # @OUT full_join_df
     joiner = Joiner(
         dish_df=dish_df,
